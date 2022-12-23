5b0c23c14820118668ca0736cd9d04e3a421acc4
UdaytejaSurampudi <119839896+UdaytejaSurampudi@users.noreply.github.com>
2022-12-23 22:42:14 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=i;j++)         {             printf("%d",j);         }         printf(" ");     }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,7 +3,7 @@ int main()
 {
     int i,j,n;
     scanf("%d",&n);
-    for(i=n;i>0;i--)
+    for(i=1;i<=n;i++)
     {
         for(j=1;j<=i;j++)
         {
@@ -13,7 +13,4 @@ int main()
 ");
     }
     
-    
 }
-
-
